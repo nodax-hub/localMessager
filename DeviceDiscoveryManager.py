7aa8f24fe02f82ffc0a1c10d301671b6004505aa
nodax-hub <ioann.ishenko.2003@gmail.com>
2024-12-16 21:14:18 +0400

    упрощение путём удаления проверки соединения с клиентами

--- DeviceDiscoveryManager.py
+++ DeviceDiscoveryManager.py
@@ -12,60 +12,6 @@ from Services.ServiceDiscovery import ServiceDiscovery, EventType
 from Services.ServicePublisher import ServicePublisher
 
 
-class ConnectionChecker:
-    def __init__(self, get_clients_callback: Callable[[], List[Service]], check_interval: int = 10):
-        """
-        :param get_clients_callback: Функция, возвращающая текущий список клиентов.
-        :param check_interval: Интервал проверки в секундах.
-        """
-        self.get_clients_callback = get_clients_callback
-        self.check_interval = check_interval
-        self.running_event = threading.Event()  # Событие для управления остановкой
-        self.running_event.set()  # Изначально событие установлено
-        self.thread = threading.Thread(target=self.run, daemon=True)
-
-        logger.debug("ConnectionChecker инициализирован.")
-
-    def start(self):
-        self.running_event.set()  # Устанавливаем событие для старта
-        self.thread.start()
-        logger.info("ConnectionChecker запущен.")
-
-    def stop(self):
-        self.running_event.clear()  # Сбрасываем событие, чтобы остановить поток
-        logger.info("ConnectionChecker остановлен.")
-
-    def run(self):
-        while self.running_event.is_set():
-
-            clients = self.get_clients_callback()
-            for service in clients:
-                if service.address:
-                    reachable = self.ping(service.address)
-                    status = "доступен" if reachable else "недоступен"
-                    # logger.debug(f"Проверка подключения к {service.name} ({service.address}): {status}")
-
-                    if not reachable:
-                        logger.warning(f"Нет соединения с {service}")
-
-            # Ждем указанное время перед следующей проверкой
-            self.running_event.wait(self.check_interval)
-
-    @staticmethod
-    def ping(address: str) -> bool:
-        """Проверяет доступность устройства по IP с помощью ICMP Echo."""
-        try:
-            # Используем системную утилиту ping
-            param = '-n' if platform.system().lower() == 'windows' else '-c'
-            command = ['ping', param, '1', address]
-            result = subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-            return result.returncode == 0
-
-        except Exception as e:
-            logger.error(f"Ошибка при пинге {address}: {e}")
-            return False
-
-
 class DeviceDiscoveryManager:
     def __init__(self,
                  publisher: ServicePublisher,
@@ -81,8 +27,6 @@ class DeviceDiscoveryManager:
         self.message_client = message_client
         self.message_server = message_server
 
-        self._connection_checker = ConnectionChecker(self.get_active_clients, check_interval=5)
-
         self.ignore_self = ignore_self
         self.clients: List[Service] = []
         self.lock = threading.Lock()
@@ -90,7 +34,6 @@ class DeviceDiscoveryManager:
     def start(self) -> None:
         self.publisher.register_service()
         self.discovery.start_discovery()
-        self._connection_checker.start()
         self.message_server.start()
 
         logger.info("DeviceDiscoveryManager запущен.")
@@ -99,7 +42,6 @@ class DeviceDiscoveryManager:
         self.message_server.stop()
         self.discovery.stop_discovery()
         self.publisher.unregister_service()
-        self._connection_checker.stop()
 
         logger.info("DeviceDiscoveryManager остановлен.")
 
